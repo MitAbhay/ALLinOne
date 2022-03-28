35dbe1755e490d011aca5d4cf75c5abf80a2a340
Abhay <leader.abhay002@gmail.com>
2022-03-28 23:09:45 +0530

    Image Added for About us Page

--- pages/about.tsx
+++ pages/about.tsx
@@ -3,12 +3,27 @@ import Header from '../components/Header'
 export default function About() {
   return (
     <>
-    <Header />
-     <div className = "max-w-7xl text-center mx-auto">
-       <div className = "text-lg">
-         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos incidunt praesentium laudantium molestias molestiae corporis, magnam sint officiis accusamus saepe nemo qui minus enim distinctio quae assumenda officia, hic quis.lorem</p>
-       </div>
-     </div>
+      <Header />
+      <div className="mx-auto max-w-7xl text-center">
+        <div className="">
+          <img src="/mypic.jpg" alt="mypic" />
+        </div>
+        <div className="text-lg">
+          <div>Abhay Kumar Mittal</div>
+          <p>
+            I am a passionate student who is pursuing my major in computer
+            science at the National Institute of Technology Hamirpur, India. I
+            enjoy solving technical problems, researching and developing new
+            technologies, designing software applications for different
+            platforms. I enjoy meeting people and working with them in a team
+            environment. I have good experience in the field of Web development,
+            where I have done various projects based on this which helps many
+            people in their technical background. And currently, I am exploring
+            the field of Machine Learning and Artificial Intelligence and
+            working on different projects and research based on it.
+          </p>
+        </div>
+      </div>
     </>
   )
 }
