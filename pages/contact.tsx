c8234347b8ea701569505f7aa190c26dcb771ce4
Abhay <leader.abhay002@gmail.com>
2022-03-30 14:25:21 +0530

    fixed bug : row string as integar

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -15,7 +15,6 @@ export default function Contact() {
   const {
     register,
     handleSubmit,
-    watch,
     formState: { errors },
   } = useForm<IFormInput>()
 
@@ -75,7 +74,6 @@ export default function Contact() {
                   {...register('message', { required: true })}
                   name="message"
                   className=" mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                  rows='3'
                   placeholder="Tell us what you're thinking about..."
                 ></textarea>
               </label>
